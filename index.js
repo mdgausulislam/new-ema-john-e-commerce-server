d431a3e6d8cbf9feea726757197d6b3ce718b781
unknown <gausulislam5@gmail.com>
2023-11-10 11:02:57 +0600

    review orser problem sove

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gxta02q.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,6 +45,28 @@ async function run() {
             const result=await productCollection.estimatedDocumentCount();
             res.send({totalProducts: result})
         })
+
+        // app.post('/productsById',async(req,res)=>{
+        //     const ids=req.body;
+        //     // const objectIds=ids.map(id=>new ObjectId(id))
+        //     const objectIds=ids.map(id=>new ObjectId(id))
+        //     const query={_id: {$in: objectIds}};
+        //     console.log(ids);
+        //     const result= await productCollection.find(query).toArray();
+        //     res.send(result)
+        // })
+
+        app.post('/productsById', async (req, res) => {
+            const ids = req.body;
+            // const objectIds = ids.map(id => new ObjectId(id));
+            const objectIds = ids.filter(id => ObjectId.isValid(id)).map(id => new ObjectId(id));
+
+            const query = { _id: { $in: objectIds } };
+            console.log(ids);
+            const result = await productCollection.find(query).toArray();
+            res.send(result);
+        })
+        
 
 
         // Send a ping to confirm a successful connection
