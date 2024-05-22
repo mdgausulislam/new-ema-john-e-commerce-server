4566a9003770d46b13c78f827cab1e53c89540a2
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2024-05-22 23:48:45 +0600

    update server

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
 // middleware 
 app.use(cors());
+
 app.use(express.json());
-
-
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -26,55 +24,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const productCollection=client.db('emaJohnDB').collection('products');
+        const productCollection = client.db('emaJohnDB').collection('products');
 
-        app.get('/products',async(req,res)=>{
+        app.get('/products', async (req, res) => {
             console.log(req.query);
 
-            const page=parseInt(req.query.page) || 0;
-            const limit =parseInt(req.query.limit) || 10;
-            const skip= page * limit;
-            const result=await productCollection.find().skip(skip).limit(limit).toArray();
+            const page = parseInt(req.query.page) || 0;
+            const limit = parseInt(req.query.limit) || 10;
+            const skip = page * limit;
+            const result = await productCollection.find().skip(skip).limit(limit).toArray();
             res.send(result);
         })
 
-        app.get('/totalProducts',async(req,res)=>{
-            const result=await productCollection.estimatedDocumentCount();
-            res.send({totalProducts: result})
+        app.get('/totalProducts', async (req, res) => {
+            const result = await productCollection.estimatedDocumentCount();
+            res.send({ totalProducts: result })
         })
-
-        // app.post('/productsById',async(req,res)=>{
-        //     const ids=req.body;
-        //     // const objectIds=ids.map(id=>new ObjectId(id))
-        //     const objectIds=ids.map(id=>new ObjectId(id))
-        //     const query={_id: {$in: objectIds}};
-        //     console.log(ids);
-        //     const result= await productCollection.find(query).toArray();
-        //     res.send(result)
-        // })
 
         app.post('/productsById', async (req, res) => {
             const ids = req.body;
-            // const objectIds = ids.map(id => new ObjectId(id));
             const objectIds = ids.filter(id => ObjectId.isValid(id)).map(id => new ObjectId(id));
-
             const query = { _id: { $in: objectIds } };
             console.log(ids);
             const result = await productCollection.find(query).toArray();
             res.send(result);
         })
-        
 
-
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
     }
 }
 run().catch(console.dir);
